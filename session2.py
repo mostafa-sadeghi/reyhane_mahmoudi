a0f948b0f7c3aa47a54aad24974a01d2dd31db3d
msa <msai.sadehi@gmail.com>
2023-06-03 20:58:14 +0330

    sadeghi

--- session2.py
+++ session2.py
@@ -1,1 +1,116 @@
-print("hello every body")
+# print("hello every body")
+# numbers    str    bool  
+# x = 1
+# print(x)
+# print(type(x))
+# x = 1.0
+# print(x)
+# print(type(x))
+# x = 1 + 2j
+# print(type(x))
+
+# هر ترکیبی از حروغ الفبای انگسیسی و اعداد و _
+# با ای ن استثنا که اولین کاراکتر نمی تواند عدد باشد
+
+# number1 = 12
+# number_1 = 14
+# _number_1 = 14
+
+# snake case
+
+# number_one = 1
+
+# camel case 
+# numberOne = 1
+
+
+# name = 'sara'
+# print(type(name))
+# name = "sara"
+# print(type(name))
+
+# name = ''
+# print(type(name))
+# name = ""
+# print(type(name))
+
+# message = "hello"
+# print(message[0])
+# print(message[1])
+# print(message[2])
+# print(message[3])
+# print(message[4])
+# print(message[5]) # Error
+ 
+
+# name = input('Enter your name: ')
+# print("hello", name)
+
+# number1 = int(input('Enter first number: ')) 
+# number2 = int(input('Enter second number: '))
+# print(number1 + number2)
+# number1 = float(input('Enter first number: ')) 
+# number2 = float(input('Enter second number: '))
+# print(number1 + number2)
+
+#  +  -   *  /    **   //    %
+
+# print(4 % 2)
+# name = input('Enter your name: ')
+# course = input('Enter your course name: ')
+# message = "hello name, welcome to course class"
+# message = "hello " + name + ", welcome to " + course + " class"
+# message = "hello %s, welcome to course class"%name
+# message = "hello %s, welcome to %s class"%(name,course)
+# message = f"hello {name}, welcome to {course} class"
+# print(message)
+
+# دو عدد را از ورودی دریافت کنید و به صورت زیر حاصلجمع را نمایش دهید
+# 1 + 2 = 3
+
+
+# message = "Hello every body. \nwelcome to python class. \nwe are going to learn everythings about python from zero to hero"
+# print(message)
+# message = '''Hello every body. 
+# welcome to python class. 
+# we are going to learn everythings about python from zero to hero'''
+# print(message)
+# message = """Hello every body. 
+# welcome to python class. 
+# we are going to learn everythings about python from zero to hero"""
+# print(message)
+
+
+# print("Display "hi" in quotes")
+# print('Display "hi" in quotes')
+# print("Display \"hi\" in quotes")
+# print('''Display "hi" in quotes''')
+
+# print(7 > 4)
+# print(3 > 4)
+
+# x = True
+# print(type(x))
+
+# y = False
+# print(type(y))
+
+# >   <     >=     <=     ==     !=
+
+# number1 = int(input('Enter a number: '))
+# number2 = int(input('Enter a number: '))
+# print(number1 > number2)
+# print(number1 < number2)
+# print(number1 >= number2)
+# print(number1 <= number2)
+# print(number1 == number2)
+# print(number1 != number2)
+
+number1 = int(input('Enter a number: '))
+number2 = int(input('Enter a number: '))
+
+if number1 > number2:
+    print("number1 is greater than number2")
+    print("ok")
+
+print("outside")
