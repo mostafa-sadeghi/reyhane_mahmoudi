1b84fe598ca08070368918af6d09bfe6731a4cde
msa <msai.sadehi@gmail.com>
2023-06-17 21:01:15 +0330

    blalal

---
+++ session3.py
@@ -0,0 +1,155 @@
+"""
+()
+**
+*   /   //  %
+"""
+# number1 = float(input('Enter a number: '))
+# number2 = float(input('Enter a number: '))
+# number3 = float(input('Enter a number: '))
+
+# max_number = max(number1, number2, number3)
+# print(f"Maximum number is : {max_number}")
+
+
+# max_number = number1
+
+# if number2 > max_number:
+#     max_number = number2
+# if number3 > max_number:
+#     max_number = number3
+
+# print(f"Maximum number is : {max_number}")
+
+
+# name = input('Enter a name: ')
+# score_1 = float(input("Enter course score: "))
+# score_2 = float(input("Enter course score: "))
+# score_3 = float(input("Enter course score: "))
+
+# average = (score_1 + score_2 + score_3)/3
+# if average >= 90:
+#     print(f"{name}'s grade is : 'A'")
+# elif average >= 80:
+#     print(f"{name}'s grade is : 'B'")
+# elif average >= 70:
+#     print(f"{name}'s grade is : 'C'")
+# elif average >= 60:
+#     print(f"{name}'s grade is : 'D'")
+# else:
+#     print(f"{name}'s grade is : 'F'")
+
+
+# grade = 63
+# result = 'passed' if grade >= 70 else 'failed'
+# print(result)
+
+
+# for i in range(3):
+#     print(i, end=" ")
+
+
+# for s in "python":
+#     print(s.upper(), end=" ")
+
+# for s in input('enter a name: '):
+#     print(s.upper(), end=" ")
+
+
+
+
+# for n in [2,3,4,5,6]:
+#     print(n)
+# for n in (2,3,4,5,6):
+#     print(n)
+
+"""
+for(int i = 0; i < 3 ; i++){
+
+}
+
+"""
+
+
+# for i in range(3):
+#     print("Hi")
+
+# i = 0
+# while i <3:
+#     print("hi")
+#     i += 1 # i = i + 1
+
+
+# running = True
+# while running:
+#     new_input = input('Enter a name or "exit" to EXIT ')
+#     if new_input == "exit":
+#         print("good bye")
+#         exit()
+#     else:
+#         print(f"you entered {new_input}")
+
+# برنامه ای بنویسید که چهار عدد از ورودی دریافت نماید و حاصل جمع اعداد زوج وارد شده را
+# محاسبه و نمایش دهد
+
+# name = ''
+# name = ""
+
+# name = "sara"
+# # name[0] = "S" # error
+
+# name += " blalalla"
+# print(name)
+
+# "sara" + " blalal"
+
+# "sara"[0] = "S"
+
+# message = "Hello every body"
+# print(message[0])
+# print(message[-1])
+# print(message[len(message)-1])
+# print(message[:3])
+# print(message[3:6])
+# print(message[3::2])
+
+
+# numbers = "123456789"
+# print(numbers[::2])
+# print(numbers[1::2])
+
+# numbers = []
+# print(type(numbers))
+
+# for i in range(3):
+#     new_number = int(input('Enter a number: '))
+#     numbers.append(new_number)
+
+# print(numbers)
+# print(sum(numbers))
+
+# TODO بدون استفاده از تابع سام، و با کمک حلقه مجموع اعداد لیست بالا را محاسبه کنید
+
+numbers = [1,2,3,4,5,6,7,8]
+
+# for number in numbers:
+#     if number % 2 == 0:
+#         numbers.remove(number)
+
+# print(numbers)
+# del numbers[2]
+# print(numbers)
+
+numbers1 = [1,2,3,4]
+numbers2 = [4,5,6,7]
+
+result = numbers1 + numbers2
+print(result)
+
+# numbers1.extend(numbers2)
+# print(numbers1)
+numbers1.append(numbers2)
+print(numbers1)
+
+
+
+
